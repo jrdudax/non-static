d65fa1229f0eadbd41bfab9efb7dbee98d20ecc7
jrdudax <john.r.duda@gmail.com>
2023-03-02 23:18:45 +0800

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var idx=0;
 var html='<meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n';
-http.createServer(function (req, res) { res.end(html+(idx++)+" : "+req.url) } ).listen(8080); 
+http.createServer(function (req, res) {
+ res.setHeader("content-type", "text/html");
+ res.end(html+(idx++)+" : "+req.url); } ).listen(8080); 
 
