5cd2581f78361bc847725c12decfca3ee04469f6
jrdudax <john.r.duda@gmail.com>
2023-03-03 15:08:50 +0900

    from work

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var idx=0;
 var html='<meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n';
+var html2='You appended the following informationt to the url: ';
 http.createServer(function (req, res) {
  res.setHeader("content-type", "text/html");
- res.end(html+(idx++)+" : "+req.url); } ).listen(8080); 
+ res.end(html+(idx++)+" : "+html2+req.url); } ).listen(8080); 
 
